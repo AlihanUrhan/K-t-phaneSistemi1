5ca72f5d5dea4a8011136a7b1e423579d7d7c299
Alihan Urhan <alihanurhnn@outlook.com>
2024-03-23 21:30:44 +0300

    23.03.2024
    GÖREV1
    
    İstenildiği üzere ''KİTAP EKLE'', ''ÜYELİK İŞLEMLERİ'' ve ''EMANET İŞLEMLERİ'' şeklinde butonlar form kısmına eklendi, renk ayarlamaları yapıldı.

--- KütüphaneSistemi1/Form1.cs
+++ KütüphaneSistemi1/Form1.cs
@@ -18,21 +18,21 @@ namespace KütüphaneSistemi1
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+         {
             KitapEkle kitapEkle = new KitapEkle();
             kitapEkle.ShowDialog();
-        }
+         }
 
         private void button2_Click(object sender, EventArgs e)
-        {
+         { 
             UyelikIslemleri uyelikIslemleri = new UyelikIslemleri();
             uyelikIslemleri.ShowDialog();
-        }
+         }
 
         private void button3_Click(object sender, EventArgs e)
-        {
+         {
             EmanetIslemleri EmanetIslemleri = new EmanetIslemleri();
             EmanetIslemleri.ShowDialog();
-        }
+         }
     }
 }
